decba21452d7cc455c8e0e80d5af1c0fd38fb69e
cyanexttime <hyperdrive098@gmail.com>
2025-05-25 22:40:29 +0700

    model

--- custom_model/model_h2/GRU_model.py
+++ custom_model/model_h2/GRU_model.py
@@ -123,7 +123,7 @@ def compile_train(model, X_train, y_train, X_val, y_val, maxEpoch, minPeers, dee
         
         # Swarm learning callback with proper validation data
         swarm_callback = SwarmCallback(
-            syncFrequency=256,              # Sync after every 10 batches
+            syncFrequency=512,              # Sync after every 10 batches
             minPeers=minPeers,             # Minimum number of peers to sync
             useAdaptiveSync=True,          # Enable adaptive sync
             adsValData=Valdata,           # Properly formatted validation data            
