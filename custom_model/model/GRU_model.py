4ae00a4e250e851d7fb5a53b0d4352bcf9952bb5
cyanexttime <hyperdrive098@gmail.com>
2025-05-29 23:00:14 +0700

    node_weightage

--- custom_model/model/GRU_model.py
+++ custom_model/model/GRU_model.py
@@ -137,7 +137,7 @@ def compile_train(model, X_train, y_train, X_val, y_val, maxEpoch, minPeers, dee
             adsValData=Valdata,           # Properly formatted validation data
             adsValBatchSize=128,    # Use the global batch_size variable
             mergeMethod='coordmedian',            # Method for model merging
-            # nodeWeightage=18,            # Weight for model averaging
+            nodeWeightage=18,            # Weight for model averaging
             # Add logging to see what's happening during training
             logDir=os.path.join(os.getenv('SCRATCH_DIR', '/platform/scratch'), 'swarm_logs')
         )
